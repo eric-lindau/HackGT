58d615d3f34c9c4af7793fd0fbb0adf511bd88ea
Kevin Park <kevin.park1217@gmail.com>
2019-10-26 18:03:17 -0400

    Update AnalyzeImage

--- analyzeImage/__init__.py
+++ analyzeImage/__init__.py
@@ -1,42 +1,65 @@
+import os, uuid, sys
+from datetime import datetime
+import azure.functions as func
 import requests
 import json
 import logging
-import azure.functions as func
-from datetime import datetime
-import os, uuid, sys
+import cognitive_face as CF
 from azure.storage.blob import BlockBlobService, PublicAccess
 
-config = {
-    "face-api": {
-        "url": "https://hackgt19.cognitiveservices.azure.com/face/v1.0/detect",
-        "key": "4e575b9e583d4c2189801f7bc8f86ce6"
-    }
-}
 
-# Get and check Face API Configuration
-face_api = config["face-api"]
-face_api_url = face_api['url']
-face_api_key = face_api['key']
+def emotion_score(anger, contempt, fear, happiness, neutral, sadness):
+    return happiness + .5 * neutral - sadness - .5 * fear - .5 * contempt - .5 * anger
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Starting Analyze Image Function")
 
+    # Get Configuration
+    config = {
+        "face-api": {
+            "url": "https://emotiontrack.cognitiveservices.azure.com/face/v1.0",
+            "key": "4e575b9e583d4c2189801f7bc8f86ce6"
+        },
+        "block-blob": {
+            "name": "hackgt19",
+            "key": "24wGa1RHd0BnemSDBbqRzvvTAB7Qy4IAN28E9de6OLR98wxnFljJXnKaBtzqJd2F53SmtNZP2NnZCPZkeL6wlQ=="
+        }
+    }
+
+    face_api = config["face-api"]
+    face_api_url = face_api['url']
+    face_api_key = face_api['key']
+
+    block_blob = config["block-blob"]
+    block_blob_name = block_blob['name']
+    block_blob_key = block_blob['key']
+
+    logging.info("Loaded Face API Configuration")
+
+    CF.BaseUrl.set(face_api_url)
+    CF.Key.set(face_api_key)
+
+    # Person ID (Unused)
     pid = req.params.get("pid")
 
-    headers = {'Ocp-Apim-Subscription-Key': face_api_key}
-    headers_disk = {
-        'Ocp-Apim-Subscription-Key': face_api_key,
-        'content-type': 'application/octet-stream'
-    }
-    params = {
-        'returnFaceId':
-        'true',
-        'returnFaceLandmarks':
-        'false',
-        'returnFaceAttributes':
-        'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
-    }
+    # headers = {'Ocp-Apim-Subscription-Key': face_api_key}
+    # headers_disk = {
+    #     'Ocp-Apim-Subscription-Key': face_api_key,
+    #     'content-type': 'application/octet-stream'
+    # }
+    # params = {
+    #     'returnFaceId':
+    #     'true',
+    #     'returnFaceLandmarks':
+    #     'false',
+    #     'returnFaceAttributes':
+    #     'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+    # }
+    # response = requests.post(face_api_url,
+    #                          params=params,
+    #                          headers=headers_disk,
+    #                          data=req.get_body())
 
     # with open("sadman.png", "rb") as image:
     #  f = image.read()
@@ -48,72 +71,83 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # response = requests.post(face_api_url, params=params,
     #                         headers=headers, json={"url": image_url})
 
-    response = requests.post(face_api_url,
-                             params=params,
-                             headers=headers_disk,
-                             data=req.get_body())
+    image = req.get_body()
 
-    anger = response.json()[0]['faceAttributes']['emotion']['anger']
-    contempt = response.json()[0]['faceAttributes']['emotion']['contempt']
-    fear = response.json()[0]['faceAttributes']['emotion']['fear']
-    happiness = response.json()[0]['faceAttributes']['emotion']['happiness']
-    neutral = response.json()[0]['faceAttributes']['emotion']['neutral']
-    sadness = response.json()[0]['faceAttributes']['emotion']['sadness']
+    attributes = (
+        'age,gender,headPose,smile,facialHair,glasses,emotion,hair,'
+        'makeup,occlusion,accessories,blur,exposure,noise')
 
-    now = datetime.now()
-    ts = datetime.timestamp(now)
+    try:
+        res = CF.face.detect(image, True, False, attributes)
 
-    def send_image(imgbytes):
-        try:
-            # Create the BlockBlockService that is used to call the Blob service for the storage account
-            block_blob_service = BlockBlobService(
-                account_name='hackgt19',
-                account_key=
-                '24wGa1RHd0BnemSDBbqRzvvTAB7Qy4IAN28E9de6OLR98wxnFljJXnKaBtzqJd2F53SmtNZP2NnZCPZkeL6wlQ=='
-            )
+        logging.info("Face API (Detection) succeeded")
+        logging.info('{} face(s) has been detected.'.format(len(res)))
+    except CF.CognitiveFaceException as exp:
+        logging.error('Response: {}. {}'.format(exp.code, exp.msg))
+        
+    time_now = datetime.now()
+    timestamp = datetime.timestamp(time_now)
+    
+    for face in res:
+        logging.info(json.dumps(face))
 
-            # Create a container called 'imageblobs'.
-            container_name = 'imageblobs'
-            block_blob_service.create_container(container_name)
-            blob_name = str(pid) + "-" + str(ts)
-            # Set the permission so the blobs are public.
-            block_blob_service.set_container_acl(
-                container_name, public_access=PublicAccess.Container)
+        face_id = face['faceId']
+        face_attributes = face['faceAttributes']
+        face_emotion = face_attributes['emotion']
 
-            # Upload the created file, use local_file_name for the blob name
-            block_blob_service.create_blob_from_bytes(container_name,
-                                                      blob_name, imgbytes)
-            # List the blobs in the container
-            # logging.info("\nList blobs in the container")
-            # generator = block_blob_service.list_blobs(container_name)
-            # for blob in generator:
-            #     logging.info("\t Blob name: " + blob.name)
+        anger = face_emotion['anger']
+        contempt = face_emotion['contempt']
+        fear = face_emotion['fear']
+        happiness = face_emotion['happiness']
+        neutral = face_emotion['neutral']
+        sadness = face_emotion['sadness']
 
-        except Exception as e:
-            logging.info(e)
+        es_value = emotion_score(anger, contempt, fear, happiness, neutral, sadness)
 
-    send_image(req.get_body())
+        logging.info("Face-ID: {}".format(face_id))
+        logging.info("Emotion Score: {}".format(es_value))
 
-    def es(ang, con, fea, hap, neu, sad):
-        return hap + .5 * neu - sad - .5 * fea - .5 * con - .5 * ang
+        params = {
+            "ts": timestamp,
+            "value": es_value,
+            "pid": pid,
+        }
 
-    value = es(anger, contempt, fear, happiness, neutral, sadness)
-    logging.info("es: " + str(value))
+        logging.info(params)
 
-    params = {
-        "ts": ts,
-        "value": value,
-        "pid": pid,
-    }
+        logging.info("Uploading Face Data to 'insertEScore' Function")
+        response = requests.post("https://swagv1.azurewebsites.net/api/insertEScore", params=params)
+        if response.status_code == 200:
+            logging.info("Upload Successful")
+        else:
+            logging.error("Error: {}".format(response.json()))
 
-    logging.info(params)
+    
+    logging.info("Uploading Image to 'imageblobs' Blob")
+    try:
+        container_name = 'imageblobs'
+        # Create the BlockBlockService that is used to call the Blob service for the storage account
+        block_blob_service = BlockBlobService(account_name=block_blob_name, account_key=block_blob_key)
 
-    res = requests.post("https://swagv1.azurewebsites.net/api/insertEScore",
-                        params=params)
+        # Create a container called 'imageblobs'.
+        block_blob_service.create_container(container_name)
+        blob_name = str(timestamp)
+        # Set the permission so the blobs are public.
+        block_blob_service.set_container_acl(
+            container_name, public_access=PublicAccess.Container)
 
-    logging.info("Emotional Score: " +
-                 str(es(anger, contempt, fear, happiness, neutral, sadness)))
-    logging.info(json.dumps(response.json()))
+        # Upload the created file, use local_file_name for the blob name
+        block_blob_service.create_blob_from_bytes(container_name, blob_name, image)
+        # List the blobs in the container
+        # logging.info("\nList blobs in the container")
+        # generator = block_blob_service.list_blobs(container_name)
+        # for blob in generator:
+        #     logging.info("\t Blob name: " + blob.name)
+        logging.info("Upload '{}' Successful".format(blob_name))
+    except Exception as e:
+        logging.error("Error: {}".format(e))
+    
+    logging.info("Finishing Analyze Image Function")
 
     # need to return es as HTTP response
-    return func.HttpResponse(json.dumps({"value": value}), status_code=200)
+    return func.HttpResponse("Success", status_code=200)
