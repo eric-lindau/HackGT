d7b02321889e59dcc3a516532f47d31373d2faa6
FUAD\fuady <fuadmyoussef@gmail.com>
2019-10-26 10:26:47 -0500

    can now take image from disk instead of URL

--- main.py
+++ main.py
@@ -13,15 +13,22 @@ face_api_url = 'https://hackgt19.cognitiveservices.azure.com/face/v1.0/detect'
 image_url = 'https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg'
 
 headers = {'Ocp-Apim-Subscription-Key': subscription_key}
-
+headers_disk = {'Ocp-Apim-Subscription-Key': subscription_key, 'content-type': 'application/octet-stream'}
 params = {
     'returnFaceId': 'true',
     'returnFaceLandmarks': 'false',
     'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
 }
-
+with open("img.png", "rb") as image:
+  f = image.read()
+  b = bytearray(f)
+  #var = b[0]
+  #print(b)
 response = requests.post(face_api_url, params=params,
-                         headers=headers, json={"url": image_url})
+                         headers=headers_disk, data=b)
+#print(response.json())
+#response = requests.post(face_api_url, params=params,
+ #                        headers=headers, json={"url": image_url})
 
 hap = response.json()[0]['faceAttributes']['emotion']['happiness']
 print(hap)
