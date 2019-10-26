e197b3eaffeb4723b4aa5186a163daaf0d286dc0
Kevin Park <kevin.park1217@gmail.com>
2019-10-26 12:55:18 -0400

    cleaning

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import requests
 import json
-
-with open('config.JSON') as config_file:
-    data = json.load(config_file)
-# set to your own subscription key value
-subscription_key = data["subKey"]
-assert subscription_key
 import logging
-
 import azure.functions as func
+
+# Load Config file
+with open("config.json") as fp:
+    config = json.load(fp)
+
+# Get and check Face API Configuration
+face_api = config["face-api"]
+assert face_api
+face_api_url = face_api['url']
+face_api_key = face_api['key']
+assert face_api_url
+assert face_api_key
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -31,13 +36,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              status_code=400
         )
 
-    # replace <My Endpoint String> with the string from your endpoint URL
-    face_api_url = 'https://hackgt19.cognitiveservices.azure.com/face/v1.0/detect'
-
-    image_url = 'https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg'
-
-    headers = {'Ocp-Apim-Subscription-Key': subscription_key}
-    headers_disk = {'Ocp-Apim-Subscription-Key': subscription_key, 'content-type': 'application/octet-stream'}
+    headers = {'Ocp-Apim-Subscription-Key': face_api_key}
+    headers_disk = {'Ocp-Apim-Subscription-Key': face_api_key, 'content-type': 'application/octet-stream'}
     params = {
         'returnFaceId': 'true',
         'returnFaceLandmarks': 'false',
@@ -45,18 +45,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     }
 
 
-    #with open("sadman.png", "rb") as image:
-     # f = image.read()
-      #b = bytearray(f)
-      #var = b[0]
-      #print(b)
+    # with open("sadman.png", "rb") as image:
+    #  f = image.read()
+    #   b = bytearray(f)
+    #   var = b[0]
+    #   print(b)
+    #
+    # print(response.json())
+    # response = requests.post(face_api_url, params=params,
+    #                         headers=headers, json={"url": image_url})
 
-    #print(response.json())
-    #response = requests.post(face_api_url, params=params,
-     #                        headers=headers, json={"url": image_url})
-
-    response = requests.post(face_api_url, params=params,
-                             headers=headers_disk, data=req_body)
+    response = requests.post(face_api_url, params=params, headers=headers_disk, data=req_body)
 
     anger = response.json()[0]['faceAttributes']['emotion']['anger']
     contempt = response.json()[0]['faceAttributes']['emotion']['contempt']
@@ -64,7 +63,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     happiness = response.json()[0]['faceAttributes']['emotion']['happiness']
     neutral = response.json()[0]['faceAttributes']['emotion']['neutral']
     sadness = response.json()[0]['faceAttributes']['emotion']['sadness']
-
 
     def es(ang, con, fea, hap, neu, sad):
         return hap + .5*neu - sad - .5*fea - .5*con - .5*ang
