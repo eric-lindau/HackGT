a334c3ef9efaffc01033223f761b70de5bacdff8
FUAD\fuady <fuadmyoussef@gmail.com>
2019-10-27 00:00:27 -0500

    plugin works

--- selfExtension/background.js
+++ selfExtension/background.js
@@ -5,42 +5,75 @@ chrome.runtime.onInstalled.addListener(() => {
   	
 });
 
-chrome.tabs.onCreated.addListener(function(tab) {
-   chrome.tabs.query({'active': true}, function(tab) {
-  		alert(new URL(tab[0].url).hostname);
-  		// Sending a receiving data in JSON format using GET method     
-	var xhr = new XMLHttpRequest();
-	var url = "https://swagv1.azurewebsites.net/api/insertMetadata?" + "pid=1";
-	xhr.open("GET", url, true);
-	xhr.setRequestHeader("Content-Type", "application/json");
-	xhr.setBody(JSON.stringify({"sites": tab}));
-	xhr.onreadystatechange = function () {
-	if (xhr.readyState === 4 && xhr.status === 200) {
-	    var json = JSON.parse(xhr.responseText);
-	    console.log(json.email + ", " + json.password);
-	}
-	};
-	xhr.send();
- });
-}); 
+// chrome.tabs.onCreated.addListener(function(tab) {
+//    chrome.tabs.query({'active': true}, function(tab) {
+//   		alert(new URL(tab[0].url).hostname);
+//   		// Sending a receiving data in JSON format using GET method 
+//   		var sites = []
+//   		var parser = document.createElement('a');
+//   		for (i = 0; i < tab.length; i++){
+//   			parser.href = tab[i];
+//   			sites.push(parser.hostname);
+//   		}
+// 	var xhr = new XMLHttpRequest();
+// 	var url = "https://swagv1.azurewebsites.net/api/insertMetadata?" + "pid=1";
+// 	xhr.open("POST", url, true);
+// 	xhr.setRequestHeader("Content-Type", "application/json");
+// 	xhr.setBody(JSON.stringify({"sites": sites}));
+// 	xhr.onreadystatechange = function () {
+// 	if (xhr.readyState === 4 && xhr.status === 200) {
+// 	    var json = JSON.parse(xhr.responseText);
+// 	    console.log(json.email + ", " + json.password);
+// 	}
+// 	};
+// 	xhr.send();
+//  });
+// }); 
+
+var prev_url = "";
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
   // how to fetch tab url using activeInfo.tabid
   chrome.tabs.get(activeInfo.tabId, function(tab){
-  	alert(tab.url);
-     console.log(tab.url);
-     // Sending a receiving data in JSON format using GET method     
-	var xhr = new XMLHttpRequest();
-	var url = "https://swagv1.azurewebsites.net/api/insertMetadata?" + "pid=1";
-	xhr.open("GET", url, true);
-	xhr.setRequestHeader("Content-Type", "application/json");
-	xhr.setBody(JSON.stringify({"sites": tab}));
-	xhr.onreadystatechange = function () {
-	if (xhr.readyState === 4 && xhr.status === 200) {
-	    var json = JSON.parse(xhr.responseText);
-	    console.log(json.email + ", " + json.password);
+	// var sites = [];
+	// var parser = document.createElement('a');
+	// for (i = 0; i < tab.length; i++){
+	// 	parser.href = tab[i];
+	// 	sites.push(parser.hostname);
+	// }
+	// console.log(tab.url);
+ //     // Sending a receiving data in JSON format using GET method     
+	// var xhr = new XMLHttpRequest();
+	// var url = "https://swagv1.azurewebsites.net/api/insertMetadata?" + "pid=1";
+	// xhr.open("POST", url, false);
+	// xhr.setRequestHeader("Content-Type", "application/json");
+	// // xhr.setBody();
+	// // xhr.onreadystatechange = function () {
+	// // 	if (xhr.readyState === 4 && xhr.status === 200) {
+	// // 	    var json = JSON.parse(xhr.responseText);
+	// // 	    console.log(json.email + ", " + json.password);
+	// // 	}
+	// // };
+	// xhr.send(JSON.stringify({"site": sites[0]}));
+ //  	alert(tab.url);
+ 	if(prev_url !== tab.url) {
+		var xhr = new XMLHttpRequest();
+		var url = "https://swagv1.azurewebsites.net/api/insertMetadata?" + "pid=1";
+		xhr.open("POST", url, false);
+		xhr.setRequestHeader("Content-Type", "application/json");
+		xhr.send(JSON.stringify({"site": tab.url}));
+		prev_url = tab.url;
 	}
-	};
-	xhr.send();
   });
 }); 
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	if(prev_url !== tab.url) {
+		var xhr = new XMLHttpRequest();
+		var url = "https://swagv1.azurewebsites.net/api/insertMetadata?" + "pid=1";
+		xhr.open("POST", url, false);
+		xhr.setRequestHeader("Content-Type", "application/json");
+		xhr.send(JSON.stringify({"site": tab.url}));
+		prev_url = tab.url;
+	}
+});
